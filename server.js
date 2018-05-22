f05db0389ef4fe8126e556cd069f5ef02ad9a7af
Igor Marakhov <imarakho@e1r5p18.unit.ua>
2018-05-22 22:49:03 +0300

    new version with tests

--- server.js
+++ server.js
@@ -9,84 +9,161 @@ var axios = require('axios');
 var b_parser = require('body-parser');
 
 
-
-// http.createServer(function (req, res) {
-  // var q = url.parse(req.url, true);
   app.use(b_parser.json());
   app.use(b_parser.urlencoded({ extended: true })); 
 
-  var filename = "./front.html";
-  var json;
-  js.jsn('./test.json', function (err, json_from_file) {
-    if(err) { throw err; }
-    console.log(json_from_file);
+  //var filename = "./front.html";
+  var json = new Object();
+  json.contracts = [];
+  var operations =[];
+  var comission = new Object();
+  comission.com = 0;
+  
+  js.jsn('./contracts.json', function (err, json_from_file) {
+    if(!err)
     json = json_from_file;
   });
 
+  js.jsn('./operations.json', function (err, json_from_file) {
+    if(!err)
+    operations = json_from_file;
+  });
+
+  js.jsn('./comission.json', function (err, json_from_file) {
+    if(!err)
+    comission = json_from_file;
+  });
 
   app.get('/', function (req, res) {
-    fs.readFile(filename, function(err, data) {
+    fs.readFile("./front.html", function(err, data) {
       if (err) {
         res.writeHead(404, {'Content-Type': 'text/html'});
         return res.end("404 Not Found");
       }  
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
-      
-      //res.write(json,contracts);
       return res.end();
     });
   });
 
   app.get('/api/contracts/', function (req, res) {
-    res.send(json);
+    
+    if(!res.send(json))
+      res.sendStatus(500);
   });
 
+  app.get('/api/operations/', function (req, res) {
+    if(!res.send(operations))
+      res.sendStatus(500);
+  });
+
+  app.get('/api/comission/', function (req, res) {
+    if(!res.send(comission))
+       res.sendStatus(500);
+       /*fs.writeFile('comission.json', JSON.stringify(comission), (err) => {
+        if (err) throw err;
+        console.log('comission has been saved!');
+      });*/
+   });
   app.post('/api/new_contract/', function (req, res) {
       var obj = req.body.contracts;
-      json.contracts.push(obj.contract_num, 
-        obj.balance);
-      console.log(json);
-      /*var obj = JSON.stringify(json) + JSON.stringify(req.body.contracts);
-      obj.splice(obj.find(']'),1);
-      obj += ']';
-      json = obj;*/
-      //json = obj;//JSON.parse(json);
-     //var obj = JSON.parse(json);
-      //json += JSON.stringify(req.body.data);
-     //fs.appendFile('test.json', JSON.parse(req.body.data));
-      //console.log(json[0]);
+      var find = json.contracts.indexOf(json.contracts.find(x => x.contract_num === req.body.contracts.contract_num));
+      if(find === -1 && Number(req.body.contract_num) >= 0 && req.body.contract_num.length === 17)
+      {
+        json.contracts.push({contract_num:obj.contract_num, 
+          balance:obj.balance});
+          fs.writeFile('contracts.json', JSON.stringify(json), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+            res.send(200);
+        });
+      }
+      else
+        res.sendStatus(500);
   });
 
+  app.post('/api/operation/', function (req, res) {
+    var find = json.contracts.indexOf(json.contracts.find(x => x.contract_num == req.body.operation.contract_num));
+    if(Number(req.body.operation.balance) > 0)
+    {
+    if(req.body.operation.oper_type === 'Снятие' )
+    {
+      res.sendStatus(200);
+      if(Number(json.contracts[find].balance) - Number(req.body.operation.balance) >= req.body.operation.card_lim)
+      {
+        json.contracts[find].balance = Number(json.contracts[find].balance) - Number(req.body.operation.balance);
+      }
+      if (Number(json.contracts[find].balance) - Number(req.body.operation.balance) < 0 && req.body.operation.card_lim)
+      {
+        comission.com += Number(req.body.operation.balance) / 100;
+        json.contracts[find].balance -= Number(req.body.operation.balance) / 100;
+        fs.writeFile('comission.json', JSON.stringify(comission), (err) => {
+        if (err) throw err;
+        console.log('comission has been saved!');
+      });
+      }
+    }
+    else if(req.body.operation.oper_type === 'Депозит')
+    {
+      console.log(find);
+      json.contracts[find].balance = Number(json.contracts[find].balance) + Number(req.body.operation.balance);
+      res.sendStatus(500);
+    }
+    operations.push({contract_num:req.body.operation.contract_num,
+      sum:req.body.operation.balance, oper_type:req.body.operation.oper_type,date:Date()})
+      fs.writeFile('operations.json', JSON.stringify(operations), (err) => {
+        if (err) throw err;
+        console.log('operations has been saved!');
+      });
+    fs.writeFile('contracts.json', JSON.stringify(json), (err) => {
+      if (err) throw err;
+      console.log('contracts has been saved!');
+     // return res.end();
+    });
+  }
+  else
+      res.sendStatus(404);
+});
+
+//token weewquewiqy343ui12y43iughewriueyoqbewrioe
+app.post('/api/cancel_operation/', function (req, res) {
+  operations = operations.reverse();
+  var find = operations.indexOf(operations.find(x => x.contract_num == req.body.operation_cancel.contract_num));
+  var find_json = json.contracts.indexOf(json.contracts.find(x => x.contract_num == req.body.operation_cancel.contract_num));
+  if(find === -1 || find_json === -1 || req.body.operation_cancel.token !== "weewquewiqy343ui12y43iughewriueyoqbewrioe")
+      res.sendStatus(404);
+  else
+  {
+    res.sendStatus(200);
+    if(operations[find].oper_type === 'Депозит')
+      json.contracts[find_json].balance = Number(json.contracts[find_json].balance) - Number(operations[find].sum);
+    else
+      json.contracts[find_json].balance = Number(json.contracts[find_json].balance) + Number(operations[find].sum);
+  //else if(json.contracts[find_json].balance < 0)
+//  {
+ //   json.contracts[find_json].balance = Number(json.contracts[find_json].balance) + Number(operations[find].sum);
+   // comission.com += (Number(json.contracts[find_json].balance) / 100);
+  //  json.contracts[find_json].balance = Number(json.contracts[find_json].balance) + (Number(json.contracts[find_json].balance) / 100); 
+  //}
+    operations.splice(find,1);
+    operations = operations.reverse();
+    fs.writeFile('operations.json', JSON.stringify(operations), (err) => {
+      if (err) throw err;
+      console.log('operations has been saved!');
+    });
+    fs.writeFile('contracts.json', JSON.stringify(json), (err) => {
+    if (err) throw err;
+    console.log('contracts has been saved!');
+    });
+  }
+});
+
   app.post('/api/add_transaction/', function (req, res) {
-    res.send(json);
+    res.send(json)
   });
 
   app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
   });
 
- // var mysql      = require('mysql');
-  /*var connection = mysql.createConnection({
-    host     : 'http://localhost:8080',
-    user     : 'root',
-    password : 'qwerty12345',
-   // database : 'contracts'
-  });*/
-
-  /*var client = mysql.createClient();
-  client.host='http://localhost:8080';
-  client.port= '3306';
-  client.user='root';
-  client.password='qwerty12345';
-  client.database='contracts';
-  
-  client.end();*/
-  //connection.connect();
-  /*
-  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
-  });*/
-  
-  //connection.end();
+  module.exports = app;
